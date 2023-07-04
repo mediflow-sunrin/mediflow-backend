3fee61636a5571f0c4c5a05e6bb777a98ece602b
PleBea <bini060315kim@gmail.com>
2023-07-04 12:13:54 +0900

    chore: Remove unused file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database.module';
@@ -14,7 +12,7 @@ import { DatabaseModule } from './database.module';
     AuthModule,
     DatabaseModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
